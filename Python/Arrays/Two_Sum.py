16a4a08ef36a61d9838d1c54532783acac243196
ASHISH U S <usashish006@gmail.com>
2026-03-19 00:13:08 +0530

    Add comments for clarity in Two sum problem
    Added comments to clarify the purpose of variables in the twoSum method.

--- Python/Arrays/Two_Sum.py
+++ Python/Arrays/Two_Sum.py
@@ -2,9 +2,9 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
         for i,j in enumerate(nums):
-            find = target - j
+            find = target - j # the number with which another one can be added to get target
             if find in seen:
-                return[seen[find],i]
+                return[seen[find],i] # returns the index of the elemnt and find whose sum is target
             else:
                 seen[j] = i
         return []
