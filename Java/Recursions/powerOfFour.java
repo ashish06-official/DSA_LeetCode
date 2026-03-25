b32a36ad607ba59ab1a6758101615fd4a93dc85d
ASHISH U S <usashish006@gmail.com>
2026-03-25 21:14:52 +0530

    Finding element is a Power Of Four

---
+++ Java/Recursions/powerOfFour.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n==1){
+            return true;
+        }
+        if ( n >= 4 && n % 4 == 0){
+            return isPowerOfFour(n/4);
+        }
+        return false;
+    }
+}
