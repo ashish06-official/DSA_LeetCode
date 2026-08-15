0618a14f6557c28440f664e3363f91ec7f47445f
ASHISH U S <usashish006@gmail.com>
2026-08-15 14:24:55 +0530

    Create kidsWithCandies.java

---
+++ Java/Arrays/kidsWithCandies.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = candies[0];
+        List<Boolean> result = new ArrayList<>();
+        for(int i = 1 ; i <candies.length ; i++){
+            if(candies[i] >= max){
+                max = candies[i];
+            }
+        }
+        for(int j = 0 ; j< candies.length ; j++){
+            if((candies[j]+extraCandies) >= max){
+                result.add(true);
+            }
+            else{
+                result.add(false);
+            }
+        }
+        return result;
+    }
+}
